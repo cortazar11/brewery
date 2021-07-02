e25254c7b6d4ebcf7d9e9bdfdbf2bf5275d71a1b
Miguel Martinez <miguel.martinez11@gmail.com>
2021-07-02 17:33:41 +0200

    good

--- server/index.js
+++ server/index.js
@@ -1,34 +1,14 @@
-<<<<<<< Updated upstream
-const express= require('express');
-const mongoose=require('mongoose');
-const cookieSession=require('cookie-session');
-const passport=require('passport');
-=======
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
->>>>>>> Stashed changes
 require('./models/User');
 require('./services/passport');
 
-<<<<<<< Updated upstream
-const authRoutes=require('./routes/authRoutes');
-const keys=require('./config/keys')
-
-const app= express();
-=======
 const authRoutes = require('./routes/authRoutes');
-const billingRoutes = require('./routes/billingRoutes');
-const detailsRoutes = require('./routes/detailsRoutes');
-
 const keys = require('./config/keys')
 
 const app = express();
-
-app.use(bodyParser.json())
->>>>>>> Stashed changes
 app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
   keys: [keys.cookieKey],
@@ -42,13 +22,10 @@ mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-<<<<<<< Updated upstream
-},()=>{
-  console.log('mongoose connectedddd')
-=======
+
 }, () => {
-  console.log('mongoose connectedddddd')
->>>>>>> Stashed changes
+  console.log('mongoose connectedddd')
+
 });
 
 authRoutes(app);
